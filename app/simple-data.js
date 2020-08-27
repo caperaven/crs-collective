1c831c341a35b2a64a471e19c1683623f1bac293
caperaven <caperaven@gmail.com>
2020-08-27 16:46:35 +0200

    initial

---
+++ app/simple-data.js
@@ -0,0 +1,18 @@
+export const data = [
+    {
+        code: "Item 1",
+        isActive: true
+    },
+    {
+        code: "Item 2",
+        isActive: false
+    },
+    {
+        code: "Item 3",
+        isActive: true
+    },
+    {
+        code: "Item 4",
+        isActive: false
+    },
+];
