0dd8574b89a9eafb07f2a37036f24b7177d026ac
caperaven <caperaven@gmail.com>
2020-09-30 14:41:21 +0200

    add filter to schema and refactor to function

--- test/filter/one-of.test.js
+++ test/filter/one-of.test.js
@@ -7,19 +7,13 @@ test("OneOfRule - execute", () => {
     expect(instance.execute({value: "d"})).toBeFalsy();
 });
 
-test("OneOfRule - execute - index", () => {
-    const instance = new OneOfRule({index: 1, value: "e"});
-    expect(instance.execute([["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"]])).toBeTruthy();
-    expect(instance.execute([["a", "b", "c"], ["g", "h", "i"] , ["d", "e", "f"]])).toBeFalsy();
-});
-
 test("OneOfRule - code", () => {
-    const instance = new OneOfRule({field: "value", value: "a"});
+    const instance = new OneOfRule({field: "value", value: ["a", "b", "c"]});
 
     const filter = new BaseSet([instance]);
     const fn = filter.toFunction({field: "value"});
     filter.dispose();
 
-    expect(fn({value: ["a", "b", "c"]})).toBeTruthy();
-    expect(fn({value: ["b", "c", "d"]})).toBeFalsy();
+    expect(fn({value: "a"})).toBeTruthy();
+    expect(fn({value: "d"})).toBeFalsy();
 });
